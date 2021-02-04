53908a09ab285a585674da33d7565ca39de48fbc
Wertzhog <ljupko.jurcevic@gmail.com>
2021-02-04 21:40:36 +0000

    Added toast template

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,6 @@
-from django.http.response import HttpResponse
-from django.shortcuts import render, redirect
-
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, render, redirect
+from products.models import Product
 # Create your views here.
 
 
@@ -13,6 +13,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ A view to add specific quantity of a product to shopping bag """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -21,7 +22,7 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
-
+        messages.success(request, f'Added {product.name} to your bag!')
     request.session['bag'] = bag
     return redirect(redirect_url)
 
@@ -34,5 +35,5 @@ def remove_from_bag(request, item_id):
     if item_id in bag:
         bag.pop(item_id)
         request.session['bag'] = bag
-    
+
     return redirect('view_bag')
